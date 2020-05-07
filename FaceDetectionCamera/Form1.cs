8951c08c7e49a8c8f7d3ed2dd1b1b46f5bd9e6ce
biragova.lyudmila <biragova.lyudmila@gmail.com>
2020-05-07 15:18:09 +0300

    Обнаружение улыбки

--- FaceDetectionCamera/Form1.cs
+++ FaceDetectionCamera/Form1.cs
@@ -10,7 +10,10 @@ using System.Windows.Forms;
 using AForge.Video;
 using AForge.Video.DirectShow;
 using Emgu.CV;
+using Emgu.Util;
+using Emgu.CV.CvEnum;
 using Emgu.CV.Structure;
+using System.IO;
 
 namespace FaceDetectionCamera
 {
@@ -22,7 +25,6 @@ namespace FaceDetectionCamera
         }
         FilterInfoCollection filter;
         VideoCaptureDevice device;
-
         private void Form1_Load(object sender, EventArgs e)
         {
             filter = new FilterInfoCollection(FilterCategory.VideoInputDevice);
@@ -40,12 +42,13 @@ namespace FaceDetectionCamera
 
         }
         static readonly CascadeClassifier cascadeClassifier = new CascadeClassifier("haarcascade_frontalface_alt_tree.xml");
+        static readonly CascadeClassifier smileClassifier = new CascadeClassifier("haarcascade_smile.xml");
         private void Device_NewFrame(object sender, NewFrameEventArgs eventArgs)
         {
             Bitmap bitmap = (Bitmap)eventArgs.Frame.Clone();
             Image<Bgr, byte> grayImage = new Image<Bgr, byte>(bitmap);
             Rectangle[] rectangles =cascadeClassifier.DetectMultiScale(grayImage, 1.2, 1);
-            foreach(Rectangle rectangle in rectangles)
+            foreach (Rectangle rectangle in rectangles)
             {
                 using(Graphics graphics=Graphics.FromImage(bitmap))
                 {
@@ -54,8 +57,22 @@ namespace FaceDetectionCamera
                         graphics.DrawRectangle(pen, rectangle);
                     }
                 }
+               
             }
-            pic.Image = bitmap;
+
+            Rectangle[] rect = smileClassifier.DetectMultiScale(grayImage, 1.8, 20);
+                foreach (Rectangle r in rect)
+                {
+                   using (Graphics graphics = Graphics.FromImage(bitmap))
+                   {
+                     using (Pen pen = new Pen(Color.Green, 1))
+                     {
+                        graphics.DrawRectangle(pen, r);
+                     }
+
+                   }
+                }
+            pic.Image = bitmap; 
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
